49e6b07ad2ed762f0531c9be447b53c7d98dfc2a
Abhiraj Sen <80464878+abhirajsen2002@users.noreply.github.com>
2022-10-18 15:21:12 +0530

    Uploaded Additional Resources

---
+++ release/moc_mainwindow.cpp
@@ -0,0 +1,237 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[31];
+    char stringdata0[532];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 10), // "closeEvent"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 12), // "QCloseEvent*"
+QT_MOC_LITERAL(4, 36, 22), // "on_actionNew_triggered"
+QT_MOC_LITERAL(5, 59, 23), // "on_actionOpen_triggered"
+QT_MOC_LITERAL(6, 83, 26), // "on_actionSave_as_triggered"
+QT_MOC_LITERAL(7, 110, 24), // "on_actionPrint_triggered"
+QT_MOC_LITERAL(8, 135, 23), // "on_actionExit_triggered"
+QT_MOC_LITERAL(9, 159, 23), // "on_actionCopy_triggered"
+QT_MOC_LITERAL(10, 183, 24), // "on_actionPaste_triggered"
+QT_MOC_LITERAL(11, 208, 22), // "on_actionCut_triggered"
+QT_MOC_LITERAL(12, 231, 23), // "on_actionUndo_triggered"
+QT_MOC_LITERAL(13, 255, 23), // "on_actionRedo_triggered"
+QT_MOC_LITERAL(14, 279, 23), // "on_actionSave_triggered"
+QT_MOC_LITERAL(15, 303, 23), // "on_actionFind_triggered"
+QT_MOC_LITERAL(16, 327, 8), // "fileInfo"
+QT_MOC_LITERAL(17, 336, 6), // "QFile*"
+QT_MOC_LITERAL(18, 343, 8), // "findNext"
+QT_MOC_LITERAL(19, 352, 4), // "text"
+QT_MOC_LITERAL(20, 357, 8), // "findPrev"
+QT_MOC_LITERAL(21, 366, 7), // "replace"
+QT_MOC_LITERAL(22, 374, 8), // "original"
+QT_MOC_LITERAL(23, 383, 6), // "target"
+QT_MOC_LITERAL(24, 390, 10), // "replaceAll"
+QT_MOC_LITERAL(25, 401, 28), // "on_actionTime_Date_triggered"
+QT_MOC_LITERAL(26, 430, 24), // "on_actionFonts_triggered"
+QT_MOC_LITERAL(27, 455, 11), // "fontChanged"
+QT_MOC_LITERAL(28, 467, 4), // "font"
+QT_MOC_LITERAL(29, 472, 26), // "on_actionCompile_triggered"
+QT_MOC_LITERAL(30, 499, 32) // "on_actionOpen_Terminal_triggered"
+
+    },
+    "MainWindow\0closeEvent\0\0QCloseEvent*\0"
+    "on_actionNew_triggered\0on_actionOpen_triggered\0"
+    "on_actionSave_as_triggered\0"
+    "on_actionPrint_triggered\0"
+    "on_actionExit_triggered\0on_actionCopy_triggered\0"
+    "on_actionPaste_triggered\0"
+    "on_actionCut_triggered\0on_actionUndo_triggered\0"
+    "on_actionRedo_triggered\0on_actionSave_triggered\0"
+    "on_actionFind_triggered\0fileInfo\0"
+    "QFile*\0findNext\0text\0findPrev\0replace\0"
+    "original\0target\0replaceAll\0"
+    "on_actionTime_Date_triggered\0"
+    "on_actionFonts_triggered\0fontChanged\0"
+    "font\0on_actionCompile_triggered\0"
+    "on_actionOpen_Terminal_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      23,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,  129,    2, 0x08 /* Private */,
+       4,    0,  132,    2, 0x08 /* Private */,
+       5,    0,  133,    2, 0x08 /* Private */,
+       6,    0,  134,    2, 0x08 /* Private */,
+       7,    0,  135,    2, 0x08 /* Private */,
+       8,    0,  136,    2, 0x08 /* Private */,
+       9,    0,  137,    2, 0x08 /* Private */,
+      10,    0,  138,    2, 0x08 /* Private */,
+      11,    0,  139,    2, 0x08 /* Private */,
+      12,    0,  140,    2, 0x08 /* Private */,
+      13,    0,  141,    2, 0x08 /* Private */,
+      14,    0,  142,    2, 0x08 /* Private */,
+      15,    0,  143,    2, 0x08 /* Private */,
+      16,    1,  144,    2, 0x08 /* Private */,
+      18,    1,  147,    2, 0x08 /* Private */,
+      20,    1,  150,    2, 0x08 /* Private */,
+      21,    2,  153,    2, 0x08 /* Private */,
+      24,    2,  158,    2, 0x08 /* Private */,
+      25,    0,  163,    2, 0x08 /* Private */,
+      26,    0,  164,    2, 0x08 /* Private */,
+      27,    1,  165,    2, 0x08 /* Private */,
+      29,    0,  168,    2, 0x08 /* Private */,
+      30,    0,  169,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 17,    2,
+    QMetaType::Void, QMetaType::QString,   19,
+    QMetaType::Void, QMetaType::QString,   19,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,   22,   23,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,   22,   23,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QFont,   28,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->closeEvent((*reinterpret_cast< QCloseEvent*(*)>(_a[1]))); break;
+        case 1: _t->on_actionNew_triggered(); break;
+        case 2: _t->on_actionOpen_triggered(); break;
+        case 3: _t->on_actionSave_as_triggered(); break;
+        case 4: _t->on_actionPrint_triggered(); break;
+        case 5: _t->on_actionExit_triggered(); break;
+        case 6: _t->on_actionCopy_triggered(); break;
+        case 7: _t->on_actionPaste_triggered(); break;
+        case 8: _t->on_actionCut_triggered(); break;
+        case 9: _t->on_actionUndo_triggered(); break;
+        case 10: _t->on_actionRedo_triggered(); break;
+        case 11: _t->on_actionSave_triggered(); break;
+        case 12: _t->on_actionFind_triggered(); break;
+        case 13: _t->fileInfo((*reinterpret_cast< QFile*(*)>(_a[1]))); break;
+        case 14: _t->findNext((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 15: _t->findPrev((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 16: _t->replace((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 17: _t->replaceAll((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 18: _t->on_actionTime_Date_triggered(); break;
+        case 19: _t->on_actionFonts_triggered(); break;
+        case 20: _t->fontChanged((*reinterpret_cast< QFont(*)>(_a[1]))); break;
+        case 21: _t->on_actionCompile_triggered(); break;
+        case 22: _t->on_actionOpen_Terminal_triggered(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 13:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QFile* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 23)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 23;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 23)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 23;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
